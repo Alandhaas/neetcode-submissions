26a27e644afc9a23f28c75bf61b79ecfc7696e77
Alan de Haas <alandehaas@hotmail.com>
2026-05-27 15:44:19 +0200

    Add: is-anagram - submission-2

---
+++ Data Structures & Algorithms/is-anagram/submission-2.java
@@ -0,0 +1,31 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if(s.length() != t.length()) return false;
+
+        Hashtable<Character, Integer> hss = new Hashtable<>();
+        Hashtable<Character, Integer> hst = new Hashtable<>();
+
+        for (char c : s.toCharArray()) {
+            if (!hss.containsKey(c)) {
+                hss.put(c, 1);
+            } else {
+                hss.put(c, hss.get(c) + 1);
+            }
+        }
+
+        for (char c : t.toCharArray()) {
+            if (!hst.containsKey(c)) {
+                hst.put(c, 1);
+            } else {
+                hst.put(c, hst.get(c) + 1);
+            }
+        }
+
+        for (Character key : hss.keySet()) {
+            if(!hss.get(key).equals(hst.get(key))) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
